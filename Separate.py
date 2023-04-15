2eed965a0b0e0d14e45c93272a6d6b8616961551
Ben Moore <ben@benhmoore.com>
2023-04-14 22:47:44 -0500

    Reorganized and added CLI interface for reduce helper

--- Separate.py
+++ Separate.py
@@ -1,14 +1,10 @@
-# Build array of lines from file, strip newlines
 import re
 import os
 import argparse
 
-
 parser = argparse.ArgumentParser(
     description='Condense a text file into a smaller file')
-filename = parser.add_argument('file_path', type=str, help='Path to the large text file')
-#parser.add_argument('lines_per_file', type=int, default=1000000,
- #                   help='Number of lines per output file')
+parser.add_argument('file_path', type=str, help='Path to the large text file')
 parser.add_argument('-o', '--output_dir', type=str, default='export',
                     help='Output directory for the smaller files (default: export)')
 
@@ -17,53 +13,27 @@ args = parser.parse_args()
 if not os.path.exists(args.output_dir):
     os.makedirs(args.output_dir)
 
-# Print the arguments and start message
 print('Arguments:', args)
 print('Starting...')
 
-
-
-filename = input("Enter a filename: ")
-
-str = "computer"
+search_string = "computer"
 pat = re.compile(r"\b\b", re.IGNORECASE)  # upper and lowercase will match
 
-mylines = []                                # Declare an empty list.
-with open (filename, 'rt', encoding='utf-8') as myfile:    # Open lorem.txt for reading text.
-    for myline in myfile:                   # For each line in the file,
-        if myline.find(str) != -1:
+mylines = []
+with open(args.file_path, 'rt', encoding='utf-8') as myfile:
+    for myline in myfile:
+        if myline.find(search_string) != -1:
             mylines.append(myline.rstrip('\n'))
-        #if pat.search(str) != None:
-            #print("Found it.")
-        #    mylines.append(myline.rstrip('\n')) # strip newline and add to list.
+
 
 def split_file(file_path, output_dir):
     with open(file_path, 'r', encoding='utf-8') as file:
-        export_file_path = f'{output_dir}/{filename}_separated.txt'
+        export_file_path = f'{output_dir}/{os.path.basename(file_path)}_separated.txt'
         current_file = open(export_file_path, 'w', encoding='utf-8')
 
         for myline in mylines:
             current_file.write(myline)
 
-#parser = argparse.ArgumentParser(
-#    description='Condense a text file into a smaller file')
-#filename = parser.add_argument('file_path', type=str, help='Path to the large text file')
-
-##parser.add_argument('lines_per_file', type=int, default=1000000,
- ##                   help='Number of lines per output file')
-
-#parser.add_argument('-o', '--output_dir', type=str, default='export',
-#                    help='Output directory for the smaller files (default: export)')
-
-#args = parser.parse_args()
-
-#if not os.path.exists(args.output_dir):
-#    os.makedirs(args.output_dir)
 
 split_file(args.file_path, args.output_dir)
 print(mylines)
-
-
-
-
-
---
+++ helper-scripts/reducer/utils.py
@@ -0,0 +1,151 @@
+import re
+import numpy as np
+from sklearn.cluster import DBSCAN
+
+
+def extract_keyword_positions(text, keywords):
+    """ Returns a list of tuples (position, keyword) for each keyword found in the text.
+
+    Example Usage:
+        >>> extract_keyword_positions("I like apples and oranges", ["apples", "oranges"])
+        [(7, "apples"), (18, "oranges")]
+        >>> extract_keyword_positions("I like apples and oranges", ["I",{"sequence": ["apples", "oranges"], "max_distance": 100}])
+        [(0, 'I'), (7, 'apples'), (18, 'oranges')]
+
+    Args:
+        text (str): The text to search.
+        keywords (list): A list of keywords to search for. A keyword can be a string or a dict. If a dict, it must have a "sequence" key and an optional "max_distance" key.
+        The "sequence" key must have a list of strings as its value. The "max_distance" key must have an integer as value, specifying the maximum character distance between the keywords in the sequence.
+
+    Returns:
+        list: A list of tuples (position, keyword) for each keyword found in the text.
+    """
+    positions = []
+    for keyword in keywords:
+        if isinstance(keyword, str):
+            for match in re.finditer(re.escape(keyword), text):
+                positions.append((match.start(), keyword))
+        elif isinstance(keyword, dict) and "sequence" in keyword:
+            sequence, max_distance = keyword["sequence"], keyword["max_distance"]
+            pattern = r"\b(?:{})\b".format("|".join(re.escape(kw)
+                                                    for kw in sequence))
+            matches = list(re.finditer(pattern, text))
+            for i in range(len(matches) - len(sequence) + 1):
+                seq_matches = matches[i:i + len(sequence)]
+                if all(seq_matches[j].group() == sequence[j] for j in range(len(sequence))):
+                    if seq_matches[-1].start() - seq_matches[0].start() <= max_distance:
+                        # Uses extend instead of append to add multiple items at once, faster than += for lists
+                        positions.extend([(match.start(), sequence[j])
+                                         for j, match in enumerate(seq_matches)])
+
+    return sorted(positions, key=lambda x: x[0])
+
+
+def cluster_positions(positions, max_distance):
+    """ Clusters positions using DBSCAN. DBSCAN is a density-based clustering algorithm that groups together points that are close to each other.
+    It is a good choice for clustering positions because it can handle outliers and does not require the number of clusters to be specified.
+
+    Example Usage:
+    >>> cluster_positions([(0, 'I'), (7, 'apples'), (18, 'oranges')], 50)
+    [0, 0, 0] # All three points are in the same cluster
+
+    Args:
+        positions (list): A list of tuples (position, keyword) for each keyword found in the text.
+        max_distance (int): The maximum distance between two positions to be considered in the same cluster.
+
+    Returns:
+        list: A list of cluster labels for each position.
+    """
+    X = np.array([[pos[0]] for pos in positions])
+    dbscan = DBSCAN(eps=max_distance, min_samples=2).fit(X)
+    return dbscan.labels_
+
+
+def extract_chunks(text, positions, clusters):
+    """ Extracts chunks from the text based on the positions and clusters.
+
+    Example Usage:
+    >>> extract_chunks("I like apples and oranges", [(0, 'I'), (7, 'apples'), (18, 'oranges')], [0, 0, 0])
+    ['I like apples and oranges']
+
+    Args:
+        text (str): The text to search.
+        positions (list): A list of tuples (position, keyword) for each keyword found in the text.
+        clusters (list): A list of cluster labels for each position.
+
+    Returns:
+        list: A list of chunks.
+    """
+    chunks = []
+    for cluster in range(max(clusters) + 1):
+        cluster_positions = [pos for i, pos in enumerate(
+            positions) if clusters[i] == cluster]
+        if cluster_positions:
+            start = cluster_positions[0][0]
+            end = cluster_positions[-1][0] + len(cluster_positions[-1][1])
+            chunks.append(text[start:end])
+    return chunks
+
+
+def count_keywords(chunk, keywords):
+    """ Counts the number of keywords in a chunk.
+
+    Example Usage:
+    >>> count_keywords("I like apples and oranges", ["apples", "oranges"])
+    2
+
+    Args:
+        chunk (str): The chunk of text to search.
+        keywords (list): A list of keywords to search for. A keyword can be a string or a dict. If a dict, it must have a "sequence" key and an optional "max_distance" key.
+
+    Returns:
+        int: The number of keywords in the chunk.
+    """
+
+    count = 0
+    for keyword in keywords:
+        if isinstance(keyword, str):
+            count += len(re.findall(re.escape(keyword), chunk))
+        elif isinstance(keyword, dict) and "sequence" in keyword:
+            sequence = keyword["sequence"]
+            pattern = r"\b(?:{})\b".format("|".join(re.escape(kw)
+                                                    for kw in sequence))
+            matches = list(re.finditer(pattern, chunk))
+            for i in range(len(matches) - len(sequence) + 1):
+                seq_matches = matches[i:i + len(sequence)]
+                if all(seq_matches[j].group() == sequence[j] for j in range(len(sequence))):
+                    count += len(sequence)
+    return count
+
+
+def chunk_text(text, keywords, max_distance):
+    """ Extracts chunks from the text based on the positions and clusters. The chunks are sorted by the number of keywords they contain so that the most relevant chunks are first.
+
+    Args:
+        text (str): The text to search.
+        keywords (list): A list of keywords to search for. A keyword can be a string or a dict. If a dict, it must have a "sequence" key and an optional "max_distance" key.
+        max_distance (int): The maximum distance between two positions to be considered in the same cluster.
+
+    Returns:
+        list: A list of chunks sorted by the number of keywords they contain.
+    """
+    positions = extract_keyword_positions(text, keywords)
+    clusters = cluster_positions(positions, max_distance)
+    chunks = extract_chunks(text, positions, clusters)
+    sorted_chunks = sorted(chunks, key=lambda chunk: count_keywords(
+        chunk, keywords), reverse=True)
+    return sorted_chunks
+
+
+# Example usage
+text = "This is a sample text with some keywords and ordered sequences. The proximity of keywords is important for chunks."
+keywords = ["This", "text", "sample",  {"sequence": [
+    "ordered", "sequences"], "max_distance": 100}]
+max_distance = 500
+
+result = chunk_text(text, keywords, max_distance)
+print(result)
+
+print(cluster_positions([(0, 'I'), (7, 'apples'), (18, 'oranges')], 50))
+print(extract_chunks("I like apples and oranges", [
+      (0, 'I'), (7, 'apples'), (18, 'oranges')], [0, 0, 0]))
